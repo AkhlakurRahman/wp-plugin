1db711bf90f3ad1819953f826c791618c5ec4862
AkhlakurRahman <akhlakur.rahmann@gmail.com>
2019-12-27 03:33:13 +0600

    callback function added for different settings

---
+++ includes/Api/Callbacks/AdminCallbacks.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Includes\Api\Callbacks;
+
+use \Includes\Base\BaseController;
+
+class AdminCallbacks extends BaseController
+{
+  public function adminDashboard()
+  {
+    return require_once("$this->plugin_path/templates/admin.php");
+  }
+
+  public function adminCPT()
+  {
+    return require_once("$this->plugin_path/templates/cpt.php");
+  }
+
+  public function adminTaxonomy()
+  {
+    return require_once("$this->plugin_path/templates/taxonomy.php");
+  }
+
+  public function adminWidget()
+  {
+    return require_once("$this->plugin_path/templates/widget.php");
+  }
+}
--- includes/Pages/Admin.php
+++ includes/Pages/Admin.php
@@ -2,62 +2,70 @@
 
 namespace Includes\Pages;
 
-use \Includes\Base\BaseController;
-use \Includes\Api\SettingsApi;
+use Includes\Api\SettingsApi;
+use Includes\Base\BaseController;
+use Includes\Api\Callbacks\AdminCallbacks;
 
 class Admin extends BaseController
 {
   public $settings;
   public $pages;
   public $subpages;
+  public $callbacks;
 
-  public function __construct()
+  public function register()
   {
     $this->settings = new SettingsApi();
+    $this->callbacks = new AdminCallbacks();
 
+    $this->setPages();
+    $this->setSubpages();
+
+    $this->settings->addPages($this->pages)->withSubPage('Dashboard')->addSubPages($this->subpages)->register();
+  }
+
+  public function setPages()
+  {
     $this->pages = [
       [
         'page_title' => 'First Plugin',
         'menu_title' => 'First Plugin',
         'capability' => 'manage_options',
         'menu_slug' => 'first_plugin',
-        'callback' => function () {
-          echo '<h1>First PPlugin</p>';
-        },
+        'callback' => array($this->callbacks, 'adminDashboard'),
         'icon_url' => 'dashicons-store',
         'position' => 110
       ]
     ];
+  }
 
+  public function setSubpages()
+  {
     $this->subpages = [
       [
         'parent_slug' => 'first_plugin',
-        'page_title' => 'First Subpage',
-        'menu_title' => 'First Subpage',
+        'page_title' => 'Custom Post Type',
+        'menu_title' => 'CPT',
         'capability' => 'manage_options',
-        'menu_slug' => 'first_subpage',
-        'callback' => function () {
-          echo '<h1>First Subpage</p>';
-        }
+        'menu_slug' => 'first_cpt',
+        'callback' => array($this->callbacks, 'adminCPT')
+      ],
+      [
+        'parent_slug' => 'first_plugin',
+        'page_title' => 'Custom Taxonomies',
+        'menu_title' => 'Taxonomies',
+        'capability' => 'manage_options',
+        'menu_slug' => 'first_taxonomies',
+        'callback' => array($this->callbacks, 'adminTaxonomy')
+      ],
+      [
+        'parent_slug' => 'first_plugin',
+        'page_title' => 'Custom Widgets',
+        'menu_title' => 'Widgets',
+        'capability' => 'manage_options',
+        'menu_slug' => 'first_widgets',
+        'callback' => array($this->callbacks, 'adminWidget')
       ]
     ];
   }
-
-  public function register()
-  {
-    // Adding menu for our plugin
-    // add_action('admin_menu', array($this, 'add_admin_pages'));
-
-    $this->settings->addPages($this->pages)->withSubPage('Dashboard')->addSubPages($this->subpages)->register();
-  }
-
-  // public function add_admin_pages()
-  // {
-  //   add_menu_page('First Plugin', 'First Plugin', 'manage_options', 'first_plugin', array($this, 'admin_index'), 'dashicons-store', 110);
-  // }
-
-  // public function admin_index()
-  // {
-  //   require_once $this->plugin_path . 'templates/admin.php';
-  // }
 }
