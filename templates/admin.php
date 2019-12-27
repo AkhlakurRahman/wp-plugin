5f0bfc2c8ccc2622bed1634f0b686f6f8b201458
AkhlakurRahman <akhlakur.rahmann@gmail.com>
2019-12-27 15:55:21 +0600

    styling added

--- templates/admin.php
+++ templates/admin.php
@@ -2,11 +2,30 @@
   <h1>First Plugin</h1>
   <?php settings_errors() ?>
 
-  <form action="options.php" method="POST">
-    <?php
-    settings_fields('first_options_group');
-    do_settings_sections('first_plugin');
-    submit_button();
-    ?>
-  </form>
+  <ul class="nav nav-tabs">
+    <li class="active"><a href="#tab-1">Manage Settings</a></li>
+    <li><a href="#tab-2">Updates</a></li>
+    <li><a href="#tab-3">About</a></li>
+  </ul>
+
+  <div class="tab-content">
+    <div id="tab-1" class="tab-pane active">
+      <form action="options.php" method="POST">
+        <?php
+        settings_fields('first_options_group');
+        do_settings_sections('first_plugin');
+        submit_button();
+        ?>
+      </form>
+    </div>
+
+    <div id="tab-2" class="tab-pane">
+      <h3>Updates</h3>
+    </div>
+
+    <div id="tab-3" class="tab-pane">
+      <h3>About</h3>
+    </div>
+  </div>
+
 </div>
