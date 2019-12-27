6279520a1fa4495cbc016aca3e23479c2276aaa4
AkhlakurRahman <akhlakur.rahmann@gmail.com>
2019-12-28 00:39:02 +0600

    code refactored

--- includes/Base/BaseController.php
+++ includes/Base/BaseController.php
@@ -11,11 +11,24 @@ class BaseController
   public $plugin_path;
   public $plugin_url;
   public $plugin;
+  public $managers = array();
 
   public function __construct()
   {
     $this->plugin_path = plugin_dir_path(dirname(__FILE__, 2));
     $this->plugin_url = plugin_dir_url(dirname(__FILE__, 2));
     $this->plugin = plugin_basename(dirname(__FILE__, 3) . '/firstplugin.php');
+
+    $this->managers = [
+      'cpt_manager' => 'Activate CPT Manager',
+      'taxonomy_manager' => 'Activate Taxonomy Manager',
+      'media_widgets' => 'Activate Media Widgets',
+      'gallery_manager' => 'Activate Gallery Manager',
+      'testimonial_manager' => 'Activate Testimonial Manager',
+      'templates_manager' => 'Activate Template Manager',
+      'login_manager' => 'Activate Login Manager',
+      'membership_manager' => 'Activate Membership Manager',
+      'chat_manager' => 'Activate Chat Manager'
+    ];
   }
 }
